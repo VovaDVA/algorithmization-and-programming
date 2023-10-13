1f4f26dc72354773a1328aa83accd0d138427d0f
Владимир Двойнишников <144770063+VovaDVA@users.noreply.github.com>
2023-10-13 18:45:35 +0300

    Update main.cpp

--- contest_03/02/main.cpp
+++ contest_03/02/main.cpp
@@ -1,1 +1,34 @@
-g
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s, '!');
+
+    stack<char> stack;
+
+    for (char c : s) {
+        switch (c) {
+            case '(': stack.push(')'); break;
+            case '[': stack.push(']'); break;
+            case '{': stack.push('}'); break;
+
+            case ')':
+            case ']':
+            case '}':
+                if (stack.empty() || stack.top() != c) {
+                    cout << "NO";
+                    return 0;
+                }
+                stack.pop();
+                break;
+            default:
+                break;
+            }
+    }
+
+    cout << (stack.empty() ? "YES" : "NO");
+}
